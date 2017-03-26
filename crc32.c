8fb479caed9f65d90e7dbb73bdb1d096eb5ffd4e
death <death@adeht.org>
2017-03-26 23:47:59 +0300

    crc32: added

---
+++ crc32.c
@@ -0,0 +1,25 @@
+/*
+ * crc32 - Print 32-bit cyclic redundancy checksum of octets from
+ * standard input.
+ */
+#include <stdio.h>
+
+#include "common.h"
+
+int main(void)
+{
+  int c;
+  unsigned int crc = 0xFFFFFFFF;
+
+  setup();
+
+  while ((c = fgetc(stdin)) != EOF) {
+    crc = crc32(crc, (unsigned char)c);
+  }
+
+  crc = ~crc;
+
+  printf("%08X\n", crc);
+
+  return 0;
+}
